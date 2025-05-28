5ac4c76cc0ed967700189e9d0e20ef97b0082660
AlbertoVazquezDiaz <20223tn083@utez.edu.mx>
2025-05-28 17:42:35 -0600

    first commit

---
+++ src/main/java/utez/edu/mx/practicadwi/PracticaDwiApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.practicadwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDwiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaDwiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/practicadwi/PracticaDwiApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.practicadwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDwiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
